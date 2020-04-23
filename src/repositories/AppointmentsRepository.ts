c4f5ca09d9293a844d0e6b9a2e25779d41bc0a78
Iury Oliveira <iury@rarolabs.com.br>
2020-04-23 00:57:55 -0300

    reflect-metadata dependency

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,77 +1,15 @@
-import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
-
-class AppointmentsRepositoy {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+@EntityRepository(Appointment)
+class AppointmentsRepositoy extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
 
     return findAppointment || null;
-  }
-
-  public findById(id: string): Appointment | null {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.id === id,
-    );
-
-    return findAppointment || null;
-  }
-
-  public findByIndex(id: string): number {
-    return this.appointments.findIndex(appointment => appointment.id === id);
-  }
-
-  public create({ provider, date }: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({ provider, date });
-
-    this.appointments.push(appointment);
-    return appointment;
-  }
-
-  public update(id: string, provider: string, date: Date): Appointment | null {
-    const appointmentIndex = this.findByIndex(id);
-
-    if (appointmentIndex < 0) {
-      return null;
-    }
-
-    const updatedAppointment: Appointment = {
-      id,
-      provider,
-      date,
-    };
-
-    this.appointments[appointmentIndex] = updatedAppointment;
-
-    return updatedAppointment;
-  }
-
-  public delete(id: string): boolean {
-    const appointmentIndex = this.findByIndex(id);
-
-    if (appointmentIndex < 0) {
-      return false;
-    }
-
-    this.appointments.splice(appointmentIndex, 1);
-
-    return true;
   }
 }
 
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,91 +1,33 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
-import FindAppointmentService from '../services/FindAppointmentService';
-import UpdateAppointmentService from '../services/UpdateAppointmentService';
-import DeleteAppointmentService from '../services/DeleteAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService(
-      appointmentsRepository,
-    );
+    const createAppointment = new CreateAppointmentService();
 
-    const appointment = createAppointment.execute({
+    const appointment = await createAppointment.execute({
       provider,
       date: parsedDate,
     });
 
     return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
-
-appointmentsRouter.get('/:id', (request, response) => {
-  try {
-    const { id } = request.params;
-
-    const findAppointmentService = new FindAppointmentService(
-      appointmentsRepository,
-    );
-
-    const appointment = findAppointmentService.execute({ id });
-
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
-
-appointmentsRouter.put('/:id', (request, response) => {
-  const { provider, date } = request.body;
-  const { id } = request.params;
-
-  try {
-    const parsedDate = startOfHour(parseISO(date));
-
-    const updateAppointment = new UpdateAppointmentService(
-      appointmentsRepository,
-    );
-
-    const updatedAppointment = updateAppointment.execute({
-      id,
-      provider,
-      date: parsedDate,
-    });
-
-    return response.json(updatedAppointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
-
-appointmentsRouter.delete('/:id', (request, response) => {
-  try {
-    const { id } = request.params;
-
-    const deleteAppointment = new DeleteAppointmentService(
-      appointmentsRepository,
-    );
-
-    deleteAppointment.execute({ id });
-    return response.send();
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
