e52f60e5707b1f7520b1228c56ce76db055ed203
Iury Oliveira <iury@rarolabs.com.br>
2020-04-23 22:44:28 -0300

    add created and updated collumn on appointments

--- src/database/migrations/1587608531682-CreateAppointments.ts
+++ src/database/migrations/1587608531682-CreateAppointments.ts
@@ -22,6 +22,16 @@ export default class CreateAppointments1587608531682
             name: 'date',
             type: 'timestamp with time zone',
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
