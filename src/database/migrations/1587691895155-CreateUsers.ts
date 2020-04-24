8d3dc03aad1f33d6901b6facf795fc9a95a867a2
Iury Oliveira <iury@rarolabs.com.br>
2020-04-23 23:48:57 -0300

    alter column nome to name

--- src/database/migrations/1587691895155-CreateUsers.ts
+++ src/database/migrations/1587691895155-CreateUsers.ts
@@ -14,7 +14,7 @@ export default class CreateUsers1587691895155 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'nome',
+            name: 'name',
             type: 'varchar',
           },
           {
