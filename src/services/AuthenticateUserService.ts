27bc27367e2d3d9043ff4f25363e85e07fd82607
Iury Oliveira <iury@rarolabs.com.br>
2020-04-25 14:21:07 -0300

    return jtw on create session

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -30,7 +32,12 @@ class AuthenticateUserService {
       throw Error('Incorrect email/password combination.');
     }
 
-    return { user };
+    const token = sign({}, '1106083f5454341300e8b03df5b4d5f0', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
